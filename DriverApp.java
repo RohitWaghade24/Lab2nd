25ecbddfccd945b1b0b45960285bc947dfc5aede
RohitWaghade24 <129169901+RohitWaghade24@users.noreply.github.com>
2023-10-03 20:33:15 +0530

    Add files via upload

--- DriverApp.java
+++ DriverApp.java
@@ -1,35 +1,54 @@
-package Currency;
+package com.transactionTargets;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class DriverApp {
-	
+
 	public static void main(String[] args) {
+		
+		int nod;
 		Scanner sc=new Scanner(System.in);
 		
-		System.out.println("Enter the no. Currency Notes in your Country");
-		int size = sc.nextInt();
+		System.out.println("Please enter the no. of days shop will be OPEN");
+		nod=sc.nextInt();
 		
-		int currency[] = new int[size];
-		System.out.println("Enter denominations of the currency notes");
-	for (int i = 0; i < currency.length; i++) {
-		currency[i] = sc.nextInt();
+		int trans[] = new int[nod];
+		int i;
+		for ( i = 0; i < trans.length; i++) {
+			System.out.println("Please enter income of day" + (i+1));
+			trans[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(trans));
 		
+		System.out.println("Please eneter no. of targets");
+		int targets=sc.nextInt();
 		
-	}
-	System.out.println("Before Sorting" + Arrays.toString(currency));
-	MergeSort ms = new MergeSort();
-	ms.sort(currency, 0, size-1);
-	System.out.println("After Sorting" + Arrays.toString(currency));
-	
-	System.out.println("Please Enter the amount");
-	int amount = sc.nextInt();
-	
-	NoteCount nc = new NoteCount();
-	nc.counting(currency,amount);
-	
-	
+		int sum;
+		int flag;
+		for(int j=1; j<=targets; j++)
+		{
+			System.out.println("Please enter for value for target" + j);
+			int singleTargets=sc.nextInt();
+			flag=0;
+			sum=0;
+			
+			for(int k=0; k<trans.length;k++)
+			{
+				sum=sum+trans[k];
+				if(sum>=singleTargets)
+				{
+					flag=1;
+					System.out.println("Target achieved on day" + (k+1));
+					break;
+				}
+			}
+			if(flag==0)
+			{
+				System.out.println("Target NOT achieved");
+			}
+		}
+
 	}
 
 }
